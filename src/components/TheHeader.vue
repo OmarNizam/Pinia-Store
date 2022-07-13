9ad67f0ef352708582d9a8b0bfd25756c61b64da
OmarNizam <eng.omarnizam@gmail.com>
2022-07-13 08:32:07 +0200

    Access Pinia Actions in the Options API

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -2,7 +2,7 @@
 // imports
 import CartWidget from "./CartWidget.vue";
 import { useAuthUserStore } from "../stores/AuthUserStore";
-import { mapState } from "pinia";
+import { mapState, mapActions } from "pinia";
 // import { mapWritableState } from "pinia";
 
 export default {
@@ -30,6 +30,11 @@ export default {
     //   user: "userName",
     // }),
   },
+  methods: {
+    ...mapActions(useAuthUserStore, {
+      toGitHub: "visitGitHubProfile",
+    }),
+  },
 };
 </script>
 
@@ -41,7 +46,7 @@ export default {
     <h1 class="text-4xl text-gray-700 font-bold">The Pineapple Stand</h1>
     <div>
       <!-- <input v-model="user" type="text" /> -->
-      <span class="mr-5">{{ user }}</span>
+      <span class="mr-5" @click="toGitHub">{{ user }}</span>
       <CartWidget class="inline-block" />
     </div>
   </header>
--- src/stores/AuthUserStore.js
+++ src/stores/AuthUserStore.js
@@ -3,7 +3,15 @@ import { defineStore } from "pinia";
 export const useAuthUserStore = defineStore("AuthUserStore", {
   state: () => {
     return {
-      userName: "Omar Nizam Aldeen",
+      userName: "OmarNizam",
     };
+  },
+  // getters: {
+  //   userName: () => "Omar from Getter",
+  // },
+  actions: {
+    visitGitHubProfile() {
+      window.open(`https://github.com/${this.userName}`, "_blank");
+    },
   },
 });
